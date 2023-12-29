cea18e926e8b11d49178398cbb7973c007b9f8ae
IReallyDontKnow101 <harshith.mulakala.549@k12.friscoisd.org>
2023-12-29 14:28:48 -0600

    enemy sprite

--- HSRizz/Assets/EnemyMovement.cs
+++ HSRizz/Assets/EnemyMovement.cs
@@ -6,9 +6,11 @@ public class EnemyMovement : MonoBehaviour
 {
 
     private GameObject player;
+    private Animator myAnim;
     // Start is called before the first frame update
     void Start()
     {
+        myAnim = GetComponent<Animator>();
         player = GameObject.FindGameObjectsWithTag("Player")[0];
     }
 
@@ -17,5 +19,9 @@ public class EnemyMovement : MonoBehaviour
     {
         float speed = 2 * Time.deltaTime;
         transform.position = Vector2.MoveTowards(transform.position, player.transform.position, speed);
+        Vector2 movement = (player.transform.position - transform.position).normalized;
+        myAnim.SetFloat("Horizontal", movement.x);
+        myAnim.SetFloat("Vertical", movement.y);
+        myAnim.SetFloat("Magnitude", movement.magnitude);
     }
 }
